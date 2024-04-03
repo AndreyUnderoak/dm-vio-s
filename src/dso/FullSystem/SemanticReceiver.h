f9b305557e410061fd29f4854e09bf30ed69f81d
Andrey Underoak <eaa4robo@gmail.com>
2024-04-03 09:34:31 -0700

    add integration of semantic object to SemanticReceiver

--- src/dso/FullSystem/SemanticReceiver.h
+++ src/dso/FullSystem/SemanticReceiver.h
@@ -12,20 +12,22 @@
 
 #include <vector>
 
+#include "FullSystem/SemanticObject.h"
+
 
 
 class SemanticReceiver {
 public:
     SemanticReceiver(int port, int max_pairs);
 	void run();
-	std::vector<int> get_px();
+	std::vector<SemanticObject> get_objects();
 private:
-    std::vector<std::vector<int>> pairs;
-	std::vector<int> px;
+	std::vector<SemanticObject> objects;
+	std::vector<int> 			datagramm_array;
     int to_int16(u_int8_t &litle, u_int8_t &big);
 	int sockfd;
 	struct sockaddr_in servaddr, cliaddr; 
 	char* buffer;
 	int max_pairs;
-	bool new_px;
+	bool new_objects;
 };

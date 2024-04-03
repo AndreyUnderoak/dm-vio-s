ef25601fdcbed055eefcd154a17227eaa5ca8026
Andrey Underoak <eaa4robo@gmail.com>
2024-04-03 09:35:10 -0700

    add semantic object to SemanticReceiver

--- src/dso/FullSystem/SemanticReceiver.cpp
+++ src/dso/FullSystem/SemanticReceiver.cpp
@@ -7,8 +7,8 @@ SemanticReceiver::SemanticReceiver(int port, int max_pairs):max_pairs(max_pairs)
 		perror("socket creation failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-
-    buffer = new char [max_pairs*2]; //not forget to delete!!!
+    //max_pairs *5(num of nums in datagramm) *2(because data sends in 2 bytes per number)
+    buffer = new char [max_pairs*5*2]; //not forget to delete!!!
     memset(&servaddr, 0, sizeof(servaddr)); 
 	memset(&cliaddr, 0, sizeof(cliaddr)); 
 
@@ -36,27 +36,34 @@ void SemanticReceiver::run(){
         n = recvfrom(sockfd, (char *)buffer, 8*max_pairs*2, 
                 MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
                 &len); 
-        std::cout<<std::endl;
         // std::cout<<"px::python --> ";
-        new_px = false;
-        px.clear();
-        for(int i = 0; i < 4; i+=2){
+        new_objects = false;
+        datagramm_array.clear();
+        objects.clear();
+        
+        for(int i = 0; i < max_pairs*5*2; i+=2){
             int res = int((unsigned char)(buffer[i])<<8 | (unsigned char)(buffer[i+1]));
-            px.push_back(res);
-            std::cout<<res<<" ";
+            datagramm_array.push_back(res);
         }
-        new_px = true;
+        for(int i = 0; i < max_pairs*5; i+=5){
+            objects.push_back(SemanticObject(datagramm_array.at(i),
+                                             datagramm_array.at(i+1),
+                                             datagramm_array.at(i+2),
+                                             datagramm_array.at(i+3),
+                                             datagramm_array.at(i+4)));
+        }
+        new_objects = true;
         std::cout<<std::endl;
     }
 }
 
-std::vector<int> SemanticReceiver::get_px(){
-    if(new_px){
-        new_px=false;
-        return px;
+std::vector<SemanticObject> SemanticReceiver::get_objects(){
+    if(new_objects){
+        new_objects=false;
+        return objects;
     }
     else{
-        std::vector<int> p;
+        std::vector<SemanticObject> p;
         return p;
     }
 }
